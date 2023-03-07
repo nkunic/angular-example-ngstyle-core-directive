8c8bba19449aae13b2f059b72bc0aaa9b696eade
nkunic <45877400+nkunic@users.noreply.github.com>
2023-03-07 11:51:07 +0100

    3 files modified, 5 added, 5 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardModule } from './card/card.module';
+import { CardModule } from './components/card/card.module';
 
 @NgModule({
   imports:      [ BrowserModule, CardModule ],
